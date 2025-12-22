0272d294c46ef2c594d3f90fffc9bfa4f8ee8724
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-12-22 17:44:42 +0600

    cors updated

--- src/util/corsOptions.js
+++ src/util/corsOptions.js
@@ -1,10 +1,11 @@
 const corsOptions = {
-    origin: [
-      "http://10.10.20.54:3000",
-      "http://localhost:3000",
-      "http://localhost:3080",
-      "http://10.10.20.43:3080"
-    ],
-    credentials: true,
-  };
+  origin: [
+    "http://10.10.20.54:3000",
+    "http://localhost:3000",
+    "http://localhost:3080",
+    "http://10.10.20.43:3080",
+    "http://3.96.86.190:4173"
+  ],
+  credentials: true,
+};
 module.exports = corsOptions;
